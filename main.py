357b1026a8cea904f155e52e77e634022240cbf1
cavemanrockx <kavin.avs@gmail.com>
2019-01-31 22:14:48 -0500

    update token
    Added more code(works)

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
 import discord
 from discord.ext import commands
 
 import chalk
 import datetime
 
-bot = commands.Bot(command_prefix="!")
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+bot = commands.Bot(command_prefix="-")
 
 TOKEN = os.getenv('token')
-
 
 
 @bot.event
 async def on_ready():
     print(chalk.green(f"{bot.user.name} Online"))
 
+
 @bot.event
 async def on_message(message):
 
-    pass
+    if message.author == bot.user:
+        return
+
+    await bot.process_commands(message)
+
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
+
+
+@bot.command()
+async def test(ctx):
+    embed = discord.Embed(title="", description="", colour=discord.Color.green())
+    embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)

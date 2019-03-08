6f2951c21d20dd3e0b63ac12ea09abf849b5b646
Kavin Adithiya <kavin.avs@gmail.com>
2019-03-08 08:47:57 -0500

    Finished images_box module
    Updated captions and image_box
    
    TODO:
    Implement image_box
    run test cases on image_box

--- cogs/memes/caption.py
+++ cogs/memes/caption.py
@@ -1,33 +1,65 @@
 from PIL import Image, ImageFont, ImageDraw, ImageOps
 import requests
 from io import BytesIO
-
+from image_utils import ImageText
 
 def two_buttons(caption1, caption2):
 
     img = Image.open("images/Two-Buttons.jpg")
-    w, h = img.size
-    print(w,h)
-    layer1 = Image.new('RGBA', img.size, (0, 0, 128, 0))
-    layer1draw = ImageDraw.Draw(layer1)
 
+    layer1_w = 187
+    layer1_h = 88
+    layer1 = Image.new('RGBA', (layer1_w, layer1_h), (0, 0, 0, 100))
+    layer1draw = ImageDraw.Draw(layer1)
+    caption1_fs = 500
     # font = ImageFont.truetype(<font-file>, <font-size>)
-    font = ImageFont.truetype("impact.ttf", 50)
+
+    layer1_font = ImageFont.truetype("impact.ttf", caption1_fs)
 
     # draw.text((x, y),"Sample Text",(r,g,b))
 
-    layer1draw.text((w/2, h/2), "Sample Text", (0, 0, 0), font=font)
+    l1w, l1h = layer1draw.multiline_textsize(caption1, layer1_font)
 
-    #layer1 = layer1.rotate(30, expand=1)
+    while (l1w > layer1_w) or (l1h > layer1_h):
+        caption1_fs -= 10
+        if caption1_fs <= 10:
+            caption1_fs = 10
+        layer1_font = ImageFont.truetype("impact.ttf", caption1_fs)
+        l1w, l1h = layer1draw.multiline_textsize(caption1, layer1_font)
 
-    img.paste(layer1, (0, 0), layer1)
+
+    # layer1draw.text((0, 0), caption1, (0, 0, 0), font=font)
+
+    layer1draw.multiline_text((0, 0), caption1, (0, 0, 0), font=layer1_font, align="center")
+
+    layer1 = layer1.rotate(16.4, expand=1)
+
+    img.paste(layer1, (48, 64), layer1)
 
     img.save('temp.jpg')
 
-    #response = requests.get(url)
-    #img = Image.open(BytesIO(response.content))
+    # response = requests.get(url)
+    # img = Image.open(BytesIO(response.content))
 
+def two_buttons2(caption1, caption2):
 
+    img = Image.open("images/Two-Buttons.jpg")
+
+    layer1_w = 187
+    layer1_h = 88
+    layer1 = ImageText((layer1_w, layer1_h), background=(0, 0, 0, 200))
+
+    #layer1.write_text_box(0, 0, caption1, box_width=layer1_w, color=(0, 0, 0),
+                          #font_filename="impact.tff", place="center")
+    #layer1.write_text_box(300, 275, caption1, box_width=200, font_filename="impact.ttf",
+                          #font_size=15, color=(0, 0, 0), place='justify')
+
+    img.paste(layer1, (48, 64, 100, 325), layer1)
+
+    img.save('temp.jpg')
+
+    # response = requests.get(url)
+    # img = Image.open(BytesIO(response.content))
 
 two_buttons("foo", "bar")
 
---
+++ cogs/memes/image_box.py
@@ -0,0 +1,104 @@
+from typing import Tuple, Optional
+from PIL import Image, ImageFont, ImageDraw
+
+
+class ImageTextBox(object):
+
+    def __init__(self, text, width, height, fontfile="impact.ttf", background=(0, 0, 0, 0), font_size=1):
+        self.text = text
+        if self.text.strip(" ") == "":
+            raise NoTextError
+
+        self.height = height
+        self.width = width
+        self.background = background
+        self.font_size = font_size
+        self.fontfile = fontfile
+        self.font = ImageFont.truetype(fontfile, self.font_size)
+
+        self.img = Image.new('RGBA', (width, height), background)
+        self.imgDraw = ImageDraw.Draw(self.img)
+
+        self.resize_to_fit()
+
+    def load(self):
+        self.img.load()
+
+    def save(self, filename):
+        self.img.save(filename)
+
+    def fit_text(self) -> Optional[Tuple]:
+        """
+        Takes the text and breaks it down into multiple lines
+
+        :returns: the height of the text
+
+        >>> text = "Hello my name is Caveman"
+        >>> img = ImageTextBox(text, 100, 200)
+        >>> img.fit_text()
+        """
+        words = self.text.split(" ")
+        lines = []
+        temp_index = 0
+
+        lines.append("")
+
+        w = 0
+        h = 0
+
+        word_too_big = False
+        for word in words:
+            w, h = self.imgDraw.textsize(lines[temp_index] + f"{word}", self.font)
+            if w >= self.width:
+                word_w, word_h = self.imgDraw.textsize(word, self.font)
+                # checks if one word is too big
+                if word_w >= self.width:
+                    word_too_big = True
+
+                lines.append(f"{word} ")
+                temp_index += 1
+            else:
+                lines[temp_index] = lines[temp_index] + f"{word} "
+
+        pos = 0
+        for i in lines:
+            pos += h
+
+        if word_too_big:
+            self.font_size -= 1
+            self.font = ImageFont.truetype(self.fontfile, self.font_size)
+            w, h = self.imgDraw.textsize(" ", self.font)
+            return self.height, lines, h
+        else:
+            return (pos + h), lines, h
+
+
+    def resize_to_fit(self):
+        """
+        Takes the text and breaks it down into multiple lines
+        and shrinks the font to fit
+
+        :returns: the height of the text
+
+        >>> text = "d"
+        >>> img = ImageTextBox(text, 100, 203)
+        >>> img.resize_to_fit()
+        """
+
+        height, lines, text_height = self.fit_text()
+        while height < self.height:
+            self.font_size += 1
+            self.font = ImageFont.truetype(self.fontfile, self.font_size)
+            height, lines, text_height = self.fit_text()
+
+        pos = 0
+        for i in lines:
+            self.imgDraw.text((0, pos), i, (0, 0, 0), font=self.font)
+            pos += text_height
+
+        self.img.save("../temp.png")
+
+
+class NoTextError(Exception):
+    """ Raised when there is no text input"""
+    pass
